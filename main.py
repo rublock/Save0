505b7b64b811ecf00d3260af084ef456a11d56e3
Mack <hypermail@yandex.ru>
2024-11-27 21:58:37 +0300

    gitignore

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def water_ground():
+	trade(Items.Empty_Tank)
 	use_item(Items.Water_Tank)
 
 def buy_tank():
@@ -7,17 +8,35 @@ def buy_tank():
 def move_drone(n, direction, func):
 	for _ in range(n):
 		for _ in range(n):
+			harvest_plant()
+			water_ground()
 			func()
 			move(direction)
 		move(East)
 
 def plant_bush():
-	use_item(Items.Water_Tank)
 	plant(Entities.Bush)
-	trade(Items.Empty_Tank)
+
+def plant_carrot():
+	trade(Items.Carrot_Seed)
+	plant(Entities.Carrots)
+
+def harvest_plant():
+	if can_harvest():
+		harvest()
+	
 	
 move_drone(4, North, till)
 
+count = 0
+
 while True:
+	print(count)
+	if count % 1 == 0:
+		trade(Items.Carrot_Seed)
+		trade(Items.Empty_Tank)
+	
+	count += 1
+
 	move_drone(4, North, plant_bush)
-	move_drone(4, North, harvest)
+	move_drone(4, North, plant_carrot)
