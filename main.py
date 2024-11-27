9493489c777e639db27047c1a061199482e5c196
Mack <hypermail@yandex.ru>
2024-11-27 19:12:14 +0300

    одна функция = 1 функционал

--- main.py
+++ main.py
@@ -1,59 +1,23 @@
-def check_if_zero_coordinates():
-	# Проверка на 0 координаты
-	return get_pos_x() == 0 and get_pos_y() == 0
+def water_ground():
+	use_item(Items.Water_Tank)
 
-def move_to_zero_coordinates(col):
-	# Двигаться пока не будут 0 координаты
-	for _ in range(col): 
-		move(South)
-		if check_if_zero_coordinates():
-			return True
-		for _ in range(col): 
-			move(West)
-			if check_if_zero_coordinates():
-				return True
-		
+def buy_tank():
+	trade(Items.Empty_Tank)
+
+def move_drone(n, direction, func):
+	for _ in range(n):
+		for _ in range(n):
+			func()
+			move(direction)
+		move(East)
+
+def plant_bush():
+	use_item(Items.Water_Tank)
+	plant(Entities.Bush)
+	trade(Items.Empty_Tank)
+	
+move_drone(4, North, till)
+
 while True:
-	if move_to_zero_coordinates(4):
-		break
-
-def move_and_plant(n, direction, item):
-	if item == None:
-		for _ in range(n):
-			if can_harvest():
-				harvest()
-			move(direction)
-		move(East)
-	elif item == Entities.Bush:
-		for _ in range(n):
-			if can_harvest():
-				harvest()
-				plant(Entities.Bush)
-			else:
-				plant(Entities.Bush)
-			move(direction)
-		move(East)
-	elif item == Entities.Carrots:
-		for _ in range(n):
-			trade(Items.Carrot_Seed)
-			trade(Items.Empty_Tank)
-			get_water()
-			if can_harvest():
-				harvest()
-				till()
-				plant(Entities.Carrots)
-				use_item(Items.Water_Tank)
-			else:
-				till()
-				plant(Entities.Carrots)
-			move(direction)
-		move(East)
-		
-def main():				
-	while True:
-		move_and_plant(4, North, Entities.Bush)
-		#move_and_plant(4, North, Entities.Carrots)
-		#move_and_plant(4, North, None)
-		
-main()
-		
+	move_drone(4, North, plant_bush)
+	move_drone(4, North, harvest)
